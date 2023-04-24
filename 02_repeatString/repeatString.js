0ae1ececae0e8afe2fb304b390939b771a785495
Brett Stine <r.brettstine@gmail.com>
2023-04-24 16:14:21 -0400

    Add my solutions for 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(myString, numberOfRepeats) {
+    if (numberOfRepeats < 0) {
+        return 'ERROR';
+    };
+    let result = "";
+    for (let i = 0; i < numberOfRepeats; i++) {
+        result += myString;
+    }
+    return result;
 };
 
 // Do not edit below this line
