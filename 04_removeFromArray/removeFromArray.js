b8fd5c45cc47a891d221302100ea8fd099e22eca
Brett Stine <r.brettstine@gmail.com>
2023-04-24 16:56:33 -0400

    Add my solutions for 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(myArray, ...myArgs) {
+    const args = [...myArgs];
+    for (let i=0; i< args.length; i++){
+        myArray = myArray.filter(value => value!==args[i]);
+    }
+    return myArray;
 };
 
 // Do not edit below this line
